ce8cf63cbdeae4e43f9d6cd21177bc323565d65b
Fábio Madeira <fbiomadeira@gmail.com>
2018-10-31 12:42:36 +0000

    JD-113 Improved Java clients quite a bunch to behave a lot more like Perl and Python clients. Improved usage messages. Also updated the mechanism in which clients are compile.

--- clientsgenerator.py
+++ clientsgenerator.py
@@ -88,11 +88,17 @@ def fetch_perl_usage(name, parameter):
                  join(textwrap.wrap(escape(parameter[1]).strip(), width=60)))).rstrip(".") + "."
 
 
-def fetch_java_usage(name, parameter):
-    return ('  --%s %s'
-            '' % (name + (19 - len(name)) * " ",
-            '\n                        '. \
-            join(textwrap.wrap(escape(parameter[1]).strip(), width=60)))).rstrip(".") + "."
+def fetch_java_options(name, parameter):
+    type = parameter[2]
+    # for val in parameter
+    if type == u'BOOLEAN':
+        return ('        allOptions.addOption("%s", "", false, "%s");'
+                '' % (name, '"\n                   + "'.
+                      join(textwrap.wrap(escape(parameter[1]).strip().replace("'", ""), width=70))))
+    else:
+        return ('        allOptions.addOption("%s", "", true, "%s");'
+                '' % (name, '"\n                   + "'.
+                      join(textwrap.wrap(escape(parameter[1]).strip().replace("'", ""), width=70))))
 
 
 def fetch_perl_types(name):
@@ -103,6 +109,11 @@ def fetch_perl_types(name):
     else {
         $tool_params{'%s'} = 0;
     }''' % (name, name, name)
+def fetch_java_usage(name, parameter):
+    return ('                                   + "  --%s %s\n'
+            '' % (name + (19 - len(name)) * " ",
+                  '\\n"\n                                   + "                        '.
+                  join(textwrap.wrap(escape(parameter[1]).strip(), width=60)).rstrip('.\\n"') + '.\\n"'))
 
 
 def fetch_java_clients(name):
@@ -110,14 +121,6 @@ def fetch_java_clients(name):
         <antcall target="jar-client">
             <param name="client" value="%s"/>
         </antcall>''' % name
-
-
-def fetch_java_message(name, parameter):
-    return u'''--%s%s\t%s%s\t%s''' % \
-           (name, (17 - len(name)) * " ",
-            parameter[2].lower().replace("sequence", "string"),
-            (7 - len(parameter[2].lower().replace("sequence", "string"))) * " ",
-            escape(parameter[1]).strip())
 
 
 def generate_client(tool, template):
@@ -131,15 +134,9 @@ def write_client(filename, contents, dir="dist"):
         fh.write(contents)
 
 
-def write_java_tool_files(filename, contents):
-    with open(filename, "w") as outlines:
-        outlines.write(contents)
-
-
 def copy_java_build_contents(src, dest="dist"):
     dest = os.path.join(os.getcwd(), dest)
     os.makedirs(os.path.join(dest, "src", "restclient", "stubs"), exist_ok=True)
-    os.makedirs(os.path.join(dest, "bin", "tools"), exist_ok=True)
     files = []
     for (dirpath, dirnames, filenames) in os.walk(src):
         files += [reduce(os.path.join, os.path.join(dirpath, file).split(os.sep)[2:])
@@ -159,6 +156,8 @@ def main(lang, client="all"):
     baseurl = "https://www.ebi.ac.uk/Tools/services/rest/"
     lang = lang.lower().split(",")
     client = client.lower().split(",")
+    required_params = ["sequence", "asequence", "bsequence",
+                       "email", "program", "stype", "database"]
     if "python" in lang:
         # Python clients
         template = Environment(loader=FileSystemLoader(u'.')) \
@@ -243,9 +242,13 @@ def main(lang, client="all"):
         # Java clients
         copy_java_build_contents(os.path.join('templates', 'java'))
 
-        template = Environment(loader=FileSystemLoader(u'.')) \
+        template_client = Environment(loader=FileSystemLoader(u'.')) \
             .get_template(os.path.join('templates', 'java', 'src',
                                        'restclient', 'RestClient.java.j2'))
+
+        template_utils = Environment(loader=FileSystemLoader(u'.')) \
+            .get_template(os.path.join('templates', 'java', 'src',
+                                       'restclient', 'ClientUtils.java.j2'))
 
         template_xml = Environment(loader=FileSystemLoader(u'.')) \
             .get_template(os.path.join('templates', 'java', 'build.xml.j2'))
@@ -261,8 +264,13 @@ def main(lang, client="all"):
                 tool = {u'id': idtool,
                         u'hostname': u'www.ebi.ac.uk',
                         u'url': u'{}{}'.format(baseurl, idtool),
-                        u'filename': os.path.join('src', 'restclient',
-                                                  u'RestClient.java'),
+                        u'filename_client': os.path.join('src', 'restclient',
+                                                         u'RestClient%s.java' % idtool),
+                        u'filename_utils': os.path.join('src', 'restclient',
+                                                        u'ClientUtils%s.java' % idtool),
+                        u'options': [],
+                        u'usage_req': [],
+                        u'usage_opt': [],
                         u'usage': [],
                         }
 
@@ -271,25 +279,22 @@ def main(lang, client="all"):
                 for option in parser[idtool]:
                     tool[option] = parser.get(idtool, option)
 
-                messages, messages2 = [], []
+                options, usage_opt, usage_req = [], [], []
                 for (name, parameter) in parameters.items():
-                    messages.append(fetch_java_message(name, parameter))
-                    messages2.append(fetch_java_usage(name, parameter))
+                    options.append(fetch_java_options(name, parameter))
+                    if name in required_params:
+                        usage_req.append(fetch_java_usage(name, parameter))
+                    else:
+                        usage_opt.append(fetch_java_usage(name, parameter))
 
-                usage = []
-                for (name, parameter) in parameters.items():
-                    usage.append(fetch_java_usage(name, parameter))
-                tool[u'usage'] = "\n".join(usage)
+                tool[u'options'] = "\n".join(options)
+                tool[u'usage_req'] = "\n".join(usage_req)
+                tool[u'usage_opt'] = "\n".join(usage_opt)
 
-                write_java_tool_files(os.path.join("dist", "bin", "tools", "%s_req.txt" % idtool),
-                                      "\n".join(messages2))
-                write_java_tool_files(os.path.join("dist", "bin", "tools", "%s_opt.txt" % idtool),
-                                      "\n".join(messages))
-                write_java_tool_files(os.path.join("dist", "bin", "tools", "%s.txt" % idtool),
-                                      "%s\n%s\n%s" % (idtool, tool["name"], tool["description"]))
-
-                contents = generate_client(tool, template)
-                write_client(tool[u'filename'], contents)
+                contents_client = generate_client(tool, template_client)
+                contents_utils = generate_client(tool, template_utils)
+                write_client(tool[u'filename_client'], contents_client)
+                write_client(tool[u'filename_utils'], contents_utils)
                 clients.append(fetch_java_clients(idtool))
                 print("Generated Java client for %s" % tool['url'], flush=True)
 
