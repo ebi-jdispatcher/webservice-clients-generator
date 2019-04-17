c0592d0e542179bf13a2174db387a1f83deea7e9
Adrian Tivey <ativey@ebi.ac.uk>
2019-04-17 09:15:26 +0100

    Add comment to script

--- templates/java/jar_header.sh
+++ templates/java/jar_header.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# This is a simple header which can make a standard executable JAR into a
+# true executable on Linux. It should be prepended to the start of the JAR
+# file. Note the 4 empty lines on the end are important
 MYSELF=`which "$0" 2>/dev/null`
 [ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
 java=java
